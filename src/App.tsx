724e92494325d745d8b01749034d7f7ec34820e6
Santosh Vijapure <vijapuresantosh@gmail.com>
2024-04-14 01:22:55 +0530

    add router with 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,39 @@ import tw, { styled } from "twin.macro";
 import Footer from "./components/Footer";
 import ActionRow from "./components/ProductAction";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./components/Error";
+
 const BannerImage = styled.img`
   ${tw`w-full`}
   height: 25rem;
   object-fit: cover;
 `;
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <React.Fragment>
+        <BannerImage src="shop.jpg" />
+        <ActionRow />
+        <ProductCatalogGrid />
+      </React.Fragment>
+    ),
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/error",
+    element: <NotFoundPage />,
+  },
+]);
+
 function App() {
   return (
     <React.Fragment>
-
       <Navbar />
-      <BannerImage src="shop.jpg"/>
-      <ActionRow/>
-      <ProductCatalogGrid />
-      <Footer/>
+      <RouterProvider router={router} />
+      <Footer />
     </React.Fragment>
   );
 }
---
+++ src/components/Error.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import tw from "twin.macro";
+
+const NotFoundPage: React.FC = () => {
+
+  return (
+    <div tw="flex flex-col items-center justify-center h-screen p-20">
+      <h1 tw="text-4xl font-bold mb-4">404 - Not Found</h1>
+      <p tw="text-lg mb-8">The page you're looking for does not exist.</p>
+      <Link to="/" tw="text-blue-500 underline">
+        Go back to home page
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
