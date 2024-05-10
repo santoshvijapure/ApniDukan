8dc074b3da700bc374e2c4560678bf69e23a4046
Santosh Vijapure <vijapuresantosh@gmail.com>
2024-05-10 23:34:28 +0530

    add legendary cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import ProductCatalogGrid from "./components/ProductCatalogGrid";
 import Navbar from "./components/Navbar";
@@ -7,8 +7,7 @@ import Footer from "./components/Footer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFoundPage from "./components/Error";
 import ProductDetailPage from "./components/ProductDetailsPage";
-
-
+import LegendaryCursor from "legendary-cursor";
 
 const router = createBrowserRouter([
   {
@@ -31,6 +30,18 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      LegendaryCursor.init({
+        lineSize: 0.015,
+        opacityDecrement: 0.55,
+        speedExpFactor: 0.8,
+        lineExpFactor: 0.6,
+        sparklesCount: 20,
+        maxOpacity: 0.40, // should be a number between [0 ... 1]
+      });
+    });
+  }, []);
   return (
     <React.Fragment>
       <Navbar />
